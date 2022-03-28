9ddaa16c679f3d3cc0d0d0024cfb0f2216e655c6
Maxwell Wang <maxwang@udel.edu>
2022-03-28 10:32:26 -0400

    header file for one kardashian

---
+++ kardashian.hpp
@@ -0,0 +1,23 @@
+#ifndef kardashian_HPP_
+#define kardashian_HPP_
+
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+class kardashian {
+	friend class kardashian_node;
+	friend class treeInterface;
+	friend class kardashian_tree;
+	string fName;
+	string lName;
+	int age;
+	string birthLocation;
+public:
+	kardashian();
+	kardashian(string f, string l, int n, string j);
+};
+
+
+
+#endif /* kardashian_HPP_ */
