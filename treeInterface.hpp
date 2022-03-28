ec843464e6f0e5f0092bd72d8c23a9a9a163f1fc
Maxwell Wang <maxwang@udel.edu>
2022-03-28 14:45:21 -0400

    added tree functions from 220

--- treeInterface.hpp
+++ treeInterface.hpp
@@ -1,5 +1,24 @@
 #ifndef treeInterface_HPP_
 #define treeInterface_HPP_
+#include <iostream>
+#include <stdlib.h>
+#include "kardashian_tree.hpp"
+using namespace std;
+
+class TreeInterface {
+	string fn;
+public:
+	kardashian_tree *tree;
+	TreeInterface(string filename);
+	void readFile();
+	void strip(string s,  int *n, string *j);
+
+	void Interface();
+//	void addPhrases();
+//	void deletePhrases();
+//	void printPhrases();
+
+};
 
 
 #endif /*treeInterface_HPP_*/
